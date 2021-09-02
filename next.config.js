e3f320fa67bc78171eaf8688caeb038859e2b9d6
oiseaubleu <michiru.shigemitsu@gmail.com>
2021-09-02 21:43:57 +0700

    画像圧縮のためのドメイン追加

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   images: {
-    domains: ["cdn2.thecatapi.com"],
+    domains: [
+      "cdn2.thecatapi.com",
+      "24.media.tumblr.com",
+      "25.media.tumblr.com",
+      "26.media.tumblr.com",
+      "27.media.tumblr.com",
+      "28.media.tumblr.com",
+    ],
   },
 }
